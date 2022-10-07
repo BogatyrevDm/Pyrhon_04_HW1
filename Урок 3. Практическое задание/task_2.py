20f2a92ab84928828c7269d3060e311816930b40
BogatyrevDm <bogatyrevdm82@gmail.com>
2022-10-07 22:42:43 +0300

    tasks for lesson 3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -22,3 +22,82 @@ f1dcaeeafeb855965535d77c55782349444b
 воспользуйтесь базой данный sqlite, postgres и т.д.
 п.с. статья на Хабре - python db-api
 """
+
+import json
+import os
+import hashlib
+
+
+def get_hash(login, password):
+    return hashlib.sha256(login.encode() + password.encode())
+
+
+def check_hash(login, password, password_check, login_hash_dict):
+    password_hash = get_hash(login, password)
+    password_hash_check = get_hash(login, password_check)
+    if password_hash.hexdigest() == password_hash_check.hexdigest():
+        print("Авторизация успешна!")
+        login_hash_dict[login] = password_hash.hexdigest()
+        return True
+    else:
+        print("Пароли не совпадают!")
+        return False
+
+
+def create_write_user(login, password, login_hash_dict, json, file_name):
+    registration_success = create_user(login, password, login_hash_dict)
+    if registration_success:
+        write_users(json, file_name, login_hash_dict)
+
+
+def check_user(login, password, hash_check, login_hash_dict):
+    password_hash = get_hash(login, password)
+    if password_hash.hexdigest() == hash_check:
+        print("Авторизация успешна!")
+    else:
+        print("Пароли не совпадают!")
+
+
+def create_user(login, password, login_hash_dict):
+    password_check = input("Введите второй раз пароль:\n")
+    return check_hash(login, password, password_check, login_hash_dict)
+
+
+def write_users(json, file_name, login_hash_dict):
+    with open(file_name, 'w', encoding='utf-8') as fw:
+        json.dump(login_hash_dict, fw, ensure_ascii=False, indent=2)
+
+
+def get_file_content(file_name):
+    with open(file_name, 'r', encoding='utf-8') as frw:
+        result_str = frw.read()
+        return result_str
+
+
+login = input("Введите логин:\n")
+password = input("Введите пароль:\n")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_NAME = 'task_3_2_users.json'
+print(BASE_DIR)
+
+full_path = os.path.join(BASE_DIR, FILE_NAME)
+# Проверим файл на существование
+if not os.path.exists(full_path):
+    # Создадим файл, если не существует
+    file = open(full_path, 'w', encoding='utf-8')
+    file.close()
+
+result_str = get_file_content(FILE_NAME)
+
+if result_str != '':
+    print(result_str)
+    result = json.loads(result_str)
+    if isinstance(result, dict):
+        user_result = result.get(login)
+        if user_result == None:
+            create_write_user(login, password, result, json, FILE_NAME)
+        else:
+            check_user(login, password, user_result, result)
+else:
+    login_hash_dict = {}
+    create_write_user(login, password, login_hash_dict, json, FILE_NAME)
