977e54a4490272979dc502c34a27ac666bf25f40
BogatyrevDm <bogatyrevdm82@gmail.com>
2022-10-10 18:28:25 +0300

    tasks for lesson 4 corrections

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -63,7 +63,22 @@ def recursive_reverse_mem(number):
         return ''
     return f'{str(number % 10)}{recursive_reverse_mem(number // 10)}'
 
-
+print('Оптимизированная функция recursive_reverse_mem')
+print(
+    timeit(
+        'recursive_reverse_mem(num_100)',
+        setup='from __main__ import recursive_reverse_mem, num_100',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_1000)',
+        setup='from __main__ import recursive_reverse_mem, num_1000',
+        number=10000))
+print(
+    timeit(
+        'recursive_reverse_mem(num_10000)',
+        setup='from __main__ import recursive_reverse_mem, num_10000',
+        number=10000))
 
 
 # Кажется, что меморизация не нужна. Цифры, передаваемые в рекурсивную функция не будут повторяться,
